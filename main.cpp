8b6d019ca65ab14a1186dc1ed174198fafe2e6ba
Patrycja Cader <patrycjacader@pop-os.localdomain>
2019-12-06 03:32:56 +0100

    BMI calculator

--- main.cpp
+++ main.cpp
@@ -3,30 +3,51 @@
 
 using namespace std;
 
+class BMI
+{
+    void calcBMI()
+    {
+        cout << "Please enter your weight (in kilograms): ";
+        double weight;
+        cin >> weight;
+
+        cout << "Please enter your height (in meters): ";
+        double height;
+        cin >> height;
+
+        double BodyMassIndex = weight / (height * height); 
+
+        cout << "Your Body Mass Index (BMI) is " << BodyMassIndex << endl; 
+
+        if (BodyMassIndex < 18.5)
+            cout << "Unfortunately, you are malnourished.\n"; 
+        else if(BodyMassIndex<25 & BodyMassIndex > 18.5)
+        {
+            cout << "Optimal" << endl;
+        }
+        else if (BodyMassIndex > 18.5)
+        {
+            cout << "You're overweight" << endl;
+        }
+        
+    }
+};
+class Food
+{
+};
+class Calories
+{
+};
+class Diet
+{
+};
+class Results
+{
+};
+class Workout
+{
+};
+
 int main()
 {
-    class BMI 
-    {
-
-    };
-    class Food
-    {
-
-    };
-    class Calories 
-    {
-
-    };
-    class Diet
-    {
-
-    };
-    class Results
-    {
-
-    };
-    class Workout
-    {
-
-    };
 }
