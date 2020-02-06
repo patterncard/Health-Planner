51c4777f4bbad76f77192d38667606cfe5f8b546
Patrycja Cader <patcad@o2.pl>
2020-02-06 22:29:23 +0100

    tracking training

--- main.cpp
+++ main.cpp
@@ -165,11 +165,7 @@ int main(int, char **)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        // if (show_demo_window)
-        //     ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        //  Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
             static float f = 0.0f;
             static int counter = 0;
@@ -216,13 +212,6 @@ int main(int, char **)
 
             ImGui::End();
         }
-
-        // 3. Show another simple window.
-        // if (show_another_window)
-        // {
-        //     ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        //     ImGui::End();
-        // }
 
         {
             static float f = 0.0f;
